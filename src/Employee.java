0f78f244187a533bd4dc6ce7cddccc6a81478f65
Imptez <imtiazmohanty@gmail.com>
2025-12-24 10:57:58 +0530

    BossCoder

--- src/Employee.java
+++ src/Employee.java
@@ -1,25 +1,4 @@
-public class Employee {
-    private String name;
-    private double salary;
-    private String department;
-
-    public Employee(String name, double salary, String department) {
-        this.name = name;
-        this.salary = salary;
-        this.department = department;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public double getSalary() {
-        return salary;
-    }
-
-    public String getDepartment() {
-        return department;
-    }
+public record Employee(String name, double salary, String department) {
 
     @Override
     public String toString() {
